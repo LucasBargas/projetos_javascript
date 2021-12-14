0f5257a99da4216528873cc3ebc23ba8bdd8f4bd
Lucas Bargas <lucas.bargasoficial@gmail.com>
2021-12-14 16:44:50 -0300

    Projeto em construção

---
+++ javascript_1/assets/js/js_modules/mobile_menu.js
@@ -0,0 +1,39 @@
+export default class MobileMenu {
+  constructor() {
+    this.menuButton = document.querySelector('.jsMobileMenu');
+    this.buttonClose = document.querySelector('.jsCloseMobile');
+    this.menuArea = document.querySelector('.jsHeaderNavSide');
+    this.navLinks = document.querySelectorAll('.jsHeaderNavLinks a');
+    this.showMenu = 'show';
+    this.events = ['click', 'touchstart'];
+
+    this.handleUserEvent = this.handleUserEvent.bind(this);
+  }
+
+  handleUserEvent(e) {
+    e.preventDefault();
+    this.menuArea.classList.toggle(this.showMenu);
+    this.buttonClose.classList.toggle(this.showMenu);
+  }
+
+  addEventsOnButtons() {
+    this.events.forEach(userEvent => {
+      this.menuButton.addEventListener(userEvent, this.handleUserEvent);
+    })
+
+    this.events.forEach(userEvent => {
+      this.buttonClose.addEventListener(userEvent, this.handleUserEvent);
+    })
+
+    this.events.forEach(userEvent => {
+      this.navLinks.forEach(link => {
+        link.addEventListener(userEvent, this.handleUserEvent);
+      })
+    })
+  }
+
+  init() {
+    this.addEventsOnButtons();
+    return this;
+  }
+}
---
+++ javascript_1/assets/js/script.js
@@ -0,0 +1,4 @@
+import MobileMenu from "./js_modules/mobile_menu.js";
+
+const mobileMenu = new MobileMenu();
+mobileMenu.init();
