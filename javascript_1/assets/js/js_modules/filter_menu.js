569f2041e9c33da70bb26bd431c08742cbd92f37
Lucas Bargas <lucas.bargasoficial@gmail.com>
2021-12-18 16:31:14 -0300

    Filtro na área de menus - desenvolvido

---
+++ javascript_1/assets/js/js_modules/filter_menu.js
@@ -0,0 +1,44 @@
+export default class filterMenu {
+  constructor() {
+    this.menuNavFilter = document.querySelectorAll('.jsMenuSection-nav li');
+    this.menuContent = document.querySelectorAll('.jsMenuSection-options ul');
+
+    this.events = ['click', 'touchstart'];
+    this.showMenu = 'showMenu';
+
+    this.menuNavFilter[0].classList.add(this.showMenu);
+    this.menuContent[0].classList.add(this.showMenu);
+  }
+
+  showMenuContent(index) {
+    this.menuContent.forEach(menu => {
+      menu.classList.remove(this.showMenu);
+    })
+    this.menuContent[index].classList.add(this.showMenu);
+  }
+
+  addClassOnLinks(e) {
+    e.preventDefault();
+
+    this.menuNavFilter.forEach(nav => {
+      nav.classList.remove(this.showMenu);
+    })
+    e.target.classList.add(this.showMenu);
+  }
+
+  addEventsOnLinks() {
+    this.events.forEach(userEvent => {
+      this.menuNavFilter.forEach((nav,index) => {
+        nav.addEventListener(userEvent, e => {
+          this.addClassOnLinks(e);
+          this.showMenuContent(index);
+        })
+      })
+    })
+  }
+
+  init() {
+    this.addEventsOnLinks();
+    return this;
+  }
+}
--- javascript_1/assets/js/script.js
+++ javascript_1/assets/js/script.js
@@ -1,8 +1,12 @@
 import MobileMenu from "./js_modules/mobile_menu.js";
 import SmoothScroll from "./js_modules/smooth_scroll.js";
+import FilterMenu from "./js_modules/filter_menu.js";
 
 const mobileMenu = new MobileMenu();
 mobileMenu.init();
 
 const smoothScroll = new SmoothScroll();
 smoothScroll.init();
+
+const filterMenu = new FilterMenu();
+filterMenu.init();
