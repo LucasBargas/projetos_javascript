498b488bb6f2833db4e0bb66cf1a34e5f1032f10
Lucas Bargas <lucas.bargasoficial@gmail.com>
2021-12-29 01:37:32 -0300

    Botão para retornar Scroll ao topo - desenvolvido

--- javascript_2/assets/js/script.js
+++ javascript_2/assets/js/script.js
@@ -1,8 +1,12 @@
 import HeaderFixed from "./js_modules/header_fixed.js";
 import MobileMenu from "./js_modules/mobile_menu.js";
+import ReturnScroll from "./js_modules/return_scroll.js";
 
 const headerFixed = new HeaderFixed();
 headerFixed.init();
 
 const mobileMenu = new MobileMenu();
 mobileMenu.init();
+
+const returnScroll = new ReturnScroll();
+returnScroll.init();
