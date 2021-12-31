f34c7d2c2fbb24db6b4f8f18e440734ac50d9784
Lucas Bargas <lucas.bargasoficial@gmail.com>
2021-12-31 15:09:14 -0300

    Seções de Time e Contato - desenvolvidas

--- javascript_2/assets/js/script.js
+++ javascript_2/assets/js/script.js
@@ -4,6 +4,7 @@ import ReturnScroll from "./js_modules/return_scroll.js";
 import SmoothScroll from "./js_modules/smooth_scroll.js";
 import AnimateNumbers from "./js_modules/animate_numbers.js";
 import PortfolioFilter from "./js_modules/portfolio_filter.js";
+import FormMessage from "./js_modules/form_message.js";
 
 const headerFixed = new HeaderFixed();
 headerFixed.init();
@@ -22,3 +23,6 @@ animateNumbers.init();
 
 const portfolioFilter = new PortfolioFilter();
 portfolioFilter.init();
+
+const formMessage = new FormMessage();
+formMessage.init();
