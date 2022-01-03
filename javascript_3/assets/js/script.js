93657341779095d899b9ceea659acb5d4da4481f
Lucas Bargas <lucas.bargasoficial@gmail.com>
2022-01-03 14:16:08 -0300

    Cores e fontes da aplicação - adicionadas

--- javascript_3/assets/js/script.js
+++ javascript_3/assets/js/script.js
@@ -1,36 +1,4 @@
-import HeaderFixed from "./js_modules/header_fixed.js";
-import MobileMenu from "./js_modules/mobile_menu.js";
-import ReturnScroll from "./js_modules/return_scroll.js";
-import SmoothScroll from "./js_modules/smooth_scroll.js";
-import AnimateNumbers from "./js_modules/animate_numbers.js";
-import PortfolioFilter from "./js_modules/portfolio_filter.js";
-import FormMessage from "./js_modules/form_message.js";
-import ScrollAnimation from "./js_modules/scroll_animation.js";
 import Modal from "./js_modules/intro_modal.js";
-
-const headerFixed = new HeaderFixed();
-headerFixed.init();
-
-const mobileMenu = new MobileMenu();
-mobileMenu.init();
-
-const returnScroll = new ReturnScroll();
-returnScroll.init();
-
-const smoothScroll = new SmoothScroll();
-smoothScroll.init();
-
-const animateNumbers = new AnimateNumbers();
-animateNumbers.init();
-
-const portfolioFilter = new PortfolioFilter();
-portfolioFilter.init();
-
-const formMessage = new FormMessage();
-formMessage.init();
-
-const scrollAnimation = new ScrollAnimation();
-scrollAnimation.init();
 
 const modal = new Modal();
 modal.init();
