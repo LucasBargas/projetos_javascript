8ecc41c1d0448b81629c47bf995c81985a64c0db
Lucas Bargas <lucas.bargasoficial@gmail.com>
2022-01-07 13:04:00 -0300

    Seção de categorias do menu Aside - desenvolvido

--- javascript_3/assets/js/js_modules/tabnav_aside.js
+++ javascript_3/assets/js/js_modules/tabnav_aside.js
@@ -2,6 +2,8 @@ export default class TabNavAside {
   constructor() {
     this.asideButton = document.querySelectorAll('.jsAsideBtn');
 
+    this.asideButtonSelect = document.querySelector('.jsAsideMenuButton');
+
     this.events = ['click', 'touchstart'];
     this.class = 'active';
 
@@ -21,6 +23,14 @@ export default class TabNavAside {
     this.events.forEach(userEvent => {
       this.asideButton.forEach(btn => {
         btn.addEventListener(userEvent, this.addClass);
+      })
+
+      this.asideButtonSelect.addEventListener(userEvent, e => {
+        const select = document.querySelector('.jsMenuSelect');
+
+        e.preventDefault();
+        e.target.classList.toggle(this.class);
+        select.classList.toggle(this.class);
       })
     })
   }
