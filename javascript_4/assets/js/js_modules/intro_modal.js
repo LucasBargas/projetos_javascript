748ad6a9be095bc183aa79ed73a38c5a199d5a30
Lucas Bargas <lucas.bargasoficial@gmail.com>
2022-02-01 23:29:25 -0300

    Adicionando arquivos do quarto projeto

---
+++ javascript_4/assets/js/js_modules/intro_modal.js
@@ -0,0 +1,36 @@
+export default class Modal {
+  constructor() {
+    this.modal = document.querySelector('.jsModal');
+    this.classActive = 'active';
+    this.event = ['touchstart', 'click']
+    document.body.style.overflow = 'hidden';
+  }
+
+  hiddenModal() {
+    this.modal.classList.remove(this.classActive);
+    document.body.style.overflow = 'auto';
+  }
+
+  addEvent() {
+    this.event.forEach(userEvent => {
+      document.addEventListener(userEvent, e => {
+        const el = e.target;
+  
+        if (el.classList.contains(this.classActive)) {
+          this.hiddenModal();
+  
+        } else if (el.classList.contains('jsCloseButton')) {
+          this.hiddenModal();
+  
+        } else if (el.classList.contains('jsModalButton')) {
+          this.hiddenModal();
+        }
+      })
+    })
+  }
+
+  init() {
+    this.addEvent();
+    return this;
+  }
+}
---
+++ javascript_4/assets/js/script.js
@@ -0,0 +1,4 @@
+import Modal from "./js_modules/intro_modal.js";
+
+const modal = new Modal();
+modal.init();
